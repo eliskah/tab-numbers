877d89a16c518d564ab8f6d78e3268de85ccb038
Eliska Hutnikova <eliska.hutnikova@gmail.com>
2015-01-01 21:08:00 +0100

    [TABS][CONTENT SCRIPT] Using content scripts to add index to all tabs in window
    - Injecting script to tabs which provides tab title to the background page
    - Getting titles using listener on background page, injecting another simple script into tabs to actually change the title, enhanced with index+1 (to reflect order used in Chrome UI)
    - Added also listeners for cases of creating or destroying tabs (for now it is causing errors, trying to inject code to destroyed tab)
    - Still can't inject numbers to special pages (extensions, new tab etc.), causing errors

--- background.js
+++ background.js
@@ -1,16 +1,31 @@
 var separator = "] "
 
-function renderTitle(tab) {
-  var title = tab.title
-  var index = tab.index + 1   // Because you need to press 1 for first page in Chrome UI
+function renderTitle(title, index) {
   var start = title.indexOf(separator) > -1 ? (title.indexOf(separator) + separator.length) : -1
 
-  return "".concat(index.toString(), separator, title.substring(start))
+  return "".concat((index+1).toString(), separator, title.substring(start))
 }
 
-chrome.commands.onCommand.addListener(function(command){
-  chrome.tabs.query({windowId: -2, highlighted: true}, function(tabs) {
-    var tab = tabs[0]
-    chrome.tabs.executeScript({ code: "document.title=".concat('"',renderTitle(tab),'"')})
+function tabs() {
+  var openTabs = [];
+  console.log("fetching tabs");
+
+  chrome.tabs.query({windowId: -2}, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      openTabs[i] = tabs[i];
+    }
+    for (var i = 0; i < openTabs.length; i++) {
+      if (openTabs[i] != null)
+        chrome.tabs.executeScript(openTabs[i].id, { file: 'content.js' }, function(response, i) {
+          chrome.runtime.onMessage.addListener(function(document, i) {
+            var title = "document.title=".concat('"', renderTitle(i.tab.title, i.tab.index), '"')
+            chrome.tabs.executeScript(i.tab.id, { code: title } )
+          })
+        })
+    }
   })
-})
+}
+
+chrome.commands.onCommand.addListener(function(command) { tabs() })
+chrome.tabs.onCreated.addListener(function(tab) { tabs(); })
+chrome.tabs.onRemoved.addListener(function(tab) { tabs(); })
