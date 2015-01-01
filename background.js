fc881d1d98cd07c5b2b56a96f2b1ac90d23057b5
Eliska Hutnikova <eliska.hutnikova@gmail.com>
2015-01-01 18:10:21 +0100

    [COMMANDS] Numbering is now activated using Commands (Alt+T default)
    - Added commands to manifest.json, changed triggering in background page
    - Still numbering only active tab

--- background.js
+++ background.js
@@ -4,11 +4,13 @@ function renderTitle(tab) {
   var title = tab.title
   var index = tab.index + 1   // Because you need to press 1 for first page in Chrome UI
   var start = title.indexOf(separator) > -1 ? (title.indexOf(separator) + separator.length) : -1
+
   return "".concat(index.toString(), separator, title.substring(start))
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-      code: "document.title=".concat('"',renderTitle(tab),'"')
-    });
-  });
+chrome.commands.onCommand.addListener(function(command){
+  chrome.tabs.query({windowId: -2, highlighted: true}, function(tabs) {
+    var tab = tabs[0]
+    chrome.tabs.executeScript({ code: "document.title=".concat('"',renderTitle(tab),'"')})
+  })
+})
