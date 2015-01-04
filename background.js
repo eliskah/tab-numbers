b02c0bd94f0affd8d7827d71a37d44a4c4b63997
Eliska Hutnikova <eliska.hutnikova@gmail.com>
2015-01-04 14:12:25 +0100

    [TABS][REFACTORING] Improved tabs numbering after exceeding 9 tabs, plus some refactoring
    - Prepending "..." instead of index on higher indexes
    - Simplified function calls, improved naming

--- background.js
+++ background.js
@@ -1,25 +1,24 @@
 var separator = "] "
 
-function renderTitle(title, index) {
+function indexTitle(tab) {
+  var index = tab.index
+  var title = tab.title
   var start = title.indexOf(separator) > -1 ? (title.indexOf(separator) + separator.length) : -1
-
+  
+  if (index > 8) { return "...".concat(separator, title) }
   return "".concat((index+1).toString(), separator, title.substring(start))
+
 }
 
 function tabs() {
-  var openTabs = [];
-  console.log("fetching tabs");
 
   chrome.tabs.query({windowId: -2}, function(tabs) {
+
     for (var i = 0; i < tabs.length; i++) {
-      openTabs[i] = tabs[i];
-    }
-    for (var i = 0; i < openTabs.length; i++) {
-      if (openTabs[i] != null)
-        chrome.tabs.executeScript(openTabs[i].id, { file: 'content.js' }, function(response, i) {
+      if (tabs[i] != null)
+        chrome.tabs.executeScript(tabs[i].id, { file: 'fetch_title.js' }, function(response, i) {
           chrome.runtime.onMessage.addListener(function(document, i) {
-            var title = "document.title=".concat('"', renderTitle(i.tab.title, i.tab.index), '"')
-            chrome.tabs.executeScript(i.tab.id, { code: title } )
+            chrome.tabs.executeScript(i.tab.id, { code: "document.title=".concat('"', indexTitle(i.tab), '"') } )
           })
         })
     }
@@ -27,5 +26,3 @@ function tabs() {
 }
 
 chrome.commands.onCommand.addListener(function(command) { tabs() })
-chrome.tabs.onCreated.addListener(function(tab) { tabs(); })
-chrome.tabs.onRemoved.addListener(function(tab) { tabs(); })
